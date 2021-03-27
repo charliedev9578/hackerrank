3b4eaca2a7cf19ff0cde611f0d0e9a23e066dc66
d3Cod3Monk <umeshmarlon@gmail.com>
2021-03-27 16:41:07 +0530

    Solve me first is done

---
+++ solvemefirst.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSum(int a , int b) {
+	return a + b;
+}
+
+int main(int argc, char const *argv[]) {
+	
+	int a;
+	int b;
+	cin >> a >> b;
+	cout << getSum(a , b) << endl;
+
+	return 0;
+}
