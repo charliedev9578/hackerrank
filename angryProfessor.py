e0bc1a30308585259ff6147561edde16024205fa
d3Cod3Monk <50435516+d3cod3monk78@users.noreply.github.com>
2022-05-19 21:26:41 +0530

    Create angryProfessor.py

---
+++ angryProfessor.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def angryProfessor(k, a):
+    count = len([x for x in a if x <= 0])
+    return 'NO' if count >= k else 'YES'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
